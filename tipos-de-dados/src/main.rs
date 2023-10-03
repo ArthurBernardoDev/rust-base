ce26baf8ed11043240877b44ef1e4fae29e31424
Arthur bernardo <arthurutschgg@gmail.com>
2023-10-03 09:03:41 -0300

    tipos de dados

---
+++ tipos-de-dados/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let x: u64 = 23; // i32
+    let f: f64 = 6.7;
+    let b: bool = false;
+}
---
+++ tipos-de-dados/target/debug/deps/tipos_de_dados-5edb50b80b051f45.d
@@ -0,0 +1,5 @@
+/Users/arthurbernardo/Documents/Projetos/estudos/Rust/tipos-de-dados/target/debug/deps/tipos_de_dados-5edb50b80b051f45.rmeta: src/main.rs
+
+/Users/arthurbernardo/Documents/Projetos/estudos/Rust/tipos-de-dados/target/debug/deps/tipos_de_dados-5edb50b80b051f45.d: src/main.rs
+
+src/main.rs:
